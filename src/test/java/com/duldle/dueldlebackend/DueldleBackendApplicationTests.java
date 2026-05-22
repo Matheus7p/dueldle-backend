a8492a695a25dbea916c01c8869940982ac91477
Matheus7p <matheus.costa.7p@gmail.com>
2026-05-22 14:07:52 -0300

    first commit

---
+++ src/test/java/com/duldle/dueldlebackend/DueldleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duldle.dueldlebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DueldleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
